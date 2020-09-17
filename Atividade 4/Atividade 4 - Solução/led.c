52ea47dc1261a826909039adb503ffed470a9eaf
gabriel-adutra <gabriel_dutra497@hotmail.com>
2020-09-16 21:58:43 -0300

    Solução da parte 2 da atividade 4

--- Atividade 4/Atividade 4 - Solução/led.c
+++ Atividade 4/Atividade 4 - Solução/led.c
@@ -12,6 +12,20 @@
 #define LED_PORT GPIOC
 #define LED_PINNBR 13
 
+/* Variável global representando quão rápido a onda triangular cresce
+   (ou decresce).  Observe o uso de volatile abaixo.  Caso não
+   entenda, pergunte ao professor */
+volatile uint8_t passo = 1;
+
+void EXTI0_IRQHandler() {
+  EXTI->PR |= (1<<0);                           // clear pending interrupt
+
+  if (GPIOA->IDR & 1)
+    passo = 3;
+  else
+    passo = 1;
+}
+
 /* Função implementado delay múltiplo de 10 microsegundos */
 void delay_dezenas_us(uint16_t dezenas) {
     int16_t i;
@@ -29,10 +43,7 @@ void delay_dezenas_us(uint16_t dezenas) {
         }
 }
 
-/* Variável global representando quão rápido a onda triangular cresce
-   (ou decresce).  Observe o uso de volatile abaixo.  Caso não
-   entenda, pergunte ao professor */
-volatile uint8_t passo = 1;
+
 
 /* Implementação de uma onda triangular variando de 0 a PERIODO */
 uint8_t onda_triangular(void) {
@@ -66,8 +77,23 @@ int main(void) {
 
     /* Inicialize aqui o que for necessário para o EXTI0 */
 
+    RCC->APB2ENR |= (RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN); // ...0001x1
+    GPIOA->CRL &= ~GPIO_CRL_CNF0_Msk; // [3] = 0
+    GPIOA->CRL |= (GPIO_CRL_CNF0_1); // [4] = 1
+
     /* Faça a configuração do que for necessário aqui */
+    EXTI->RTSR |= EXTI_RTSR_TR0; // Rising trigger enabled
+    EXTI->FTSR |= EXTI_FTSR_TR0; // Falling trigger enabled
+    AFIO->EXTICR[0] = 0; // Enable interrupt from port A (...0000)
+
+    EXTI->IMR |= EXTI_IMR_MR0;
+
+    NVIC->ISER[EXTI0_IRQn >> 5] = (1U << (EXTI0_IRQn & 0x1F));
+    NVIC->IP[EXTI0_IRQn] |= (0x4 << 4); /* 0x4 é prioridade média */
+
     /* Habilita interrupçoes globalmente */
+    __enable_irq();
+
     /* Habilitando o relógio da porta C para o led e configura bit 13
        da porta GPIOC como output 10 MHz para o led */
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
@@ -75,8 +101,6 @@ int main(void) {
     LED_PORT->CRH |= (GPIO_CRH_MODE13_0);
     LED_PORT->BSRR = (1 << LED_PINNBR);
 
-
-    // __enable_irq();
 
     /* Implementação de uma onda quadrada com ciclo de trabalho
        variável. O ciclo de trabalho varia a cada QTDE_PULSOS pulsos
